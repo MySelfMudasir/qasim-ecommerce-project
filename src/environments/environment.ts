9f2ba6eba9b0b5786ba8238d58621bc6720a4cc4
Mudasir Maqbool <mudasir.maqbool@ablfunds.com>
2026-04-03 09:37:07 +0500

    fix(package): update start script to use server.mjs for production refactor(environment): remove Firebase configuration from development environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,7 @@
 export const environment: any = {
   production: false,
 
-  firebase: {
-    apiKey: "AIzaSyCoNmhSKPRDtyiWCMtoRvBgSKVETQCPWjI",
-    authDomain: "mudasir-s-project.firebaseapp.com",
-    databaseURL: "https://mudasir-s-project-default-rtdb.firebaseio.com",
-    projectId: "mudasir-s-project",
-    storageBucket: "mudasir-s-project.firebasestorage.app",
-    messagingSenderId: "996930084257",
-    appId: "1:996930084257:web:2a62e6a92b55fba57dbfe0",
-    measurementId: "G-GF9LJ4LKDC"
-  }
+
 
 
 
