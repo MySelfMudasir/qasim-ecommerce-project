f36c025240ba26cc8771721dcc0de81f23dd200f
Mudasir Maqbool <mudasir.maqbool@ablfunds.com>
2026-04-12 09:54:21 +0500

    feat(layout): implement main layout component with HTML and SCSS files fix(header): update button imports and adjust HTML structure for consistency

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -3,14 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { HeaderActions } from '../header-actions/header-actions';
 import { MatIcon } from '@angular/material/icon';
 import { SidenavService } from '../../services/sidenav';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatButtonModule, MatIconButton } from '@angular/material/button';
 import { RouterLink } from "@angular/router";
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { SearchLoadingService } from '../../services/search-loading';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, HeaderActions, MatIcon, MatButton, RouterLink, MatProgressBarModule],
+  imports: [MatToolbarModule, HeaderActions, MatIcon, MatButtonModule, RouterLink, MatProgressBarModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
